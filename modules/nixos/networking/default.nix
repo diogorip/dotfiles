2074278457f20856c5b3a41fd03ffc5a324faf35
Diogo <hi@luvsick.gg>
2025-09-10 17:38:38 +0100

    nixos/networking: add default nameservers

--- modules/nixos/networking/default.nix
+++ modules/nixos/networking/default.nix
@@ -10,5 +10,12 @@
     hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
 
     networkmanager.enable = true;
+
+    nameservers = [
+      "1.1.1.1"
+      "8.8.8.8"
+      "2606:4700:4700::1111"
+      "2001:4860:4860::8844"
+    ];
   };
 }
