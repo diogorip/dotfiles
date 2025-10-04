c00c14f0be65646d2d67c948ce5870e2204753cc
Diogo <hi@luvsick.gg>
2025-10-04 20:25:43 +0100

    fix matrix db

--- modules/nixos/services/matrix.nix
+++ modules/nixos/services/matrix.nix
@@ -2,6 +2,7 @@
   self,
   lib,
   config,
+  pkgs,
   ...
 }:
 let
@@ -70,11 +71,13 @@ in
       };
 
       postgresql = {
-        ensureDatabases = [ "matrix-synapse" ];
-        ensureUsers = lib.singleton {
-          name = "matrix-synapse";
-          ensureDBOwnership = true;
-        };
+        initialScript = pkgs.writeText "synapse-init.sql" ''
+          CREATE ROLE "matrix-synapse" WITH LOGIN PASSWORD 'synapse';
+          CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
+            TEMPLATE template0
+            LC_COLLATE = "C"
+            LC_CTYPE = "C";
+        '';
       };
 
       caddy.virtualHosts.${domain} = {
